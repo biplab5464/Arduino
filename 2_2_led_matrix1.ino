79fe5290f391f31638809c139335f2bc7e16e5cd
Biplab Mochan Gartia <45629823+biplab5464@users.noreply.github.com>
2021-02-23 14:08:16 +0530

    Update 2_2_led_matrix1.ino

--- 2_2_led_matrix1.ino
+++ 2_2_led_matrix1.ino
@@ -83,14 +83,36 @@ void allOff()
 
 void loop()
 {
-  ledA2();
-  delay(5);
-  allOff();
-  ledB1();
-  delay(5);
-  allOff();
-  ledB2();
-  delay(5);
-  allOff();
+  int i;
+  for(i=0;i<3;i++)
+  {
+    allLedOn();
+    delay(200);
+    allOff();
+    delay(100);
+  }
+
+  ledA();
+  delay(500);
+  ledB();
+  delay(500);
+  led1();
+  delay(500);
+  led2();
+  delay(500);
+  led1();
+  delay(500);
+  ledB();
+  delay(500);
+
+  
+  for(i=0;i<2;i++)
+  {
+    allLedOn();
+    delay(200);
+    allOff();
+    delay(100); 
+  }
+  delay(2000);
    
 }
